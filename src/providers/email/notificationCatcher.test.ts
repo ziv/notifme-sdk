7a9bc3ea63144def915b5b0b2c28a269af7c65b7
Ziv <ziv.perry@binah.ai>
2019-06-13 19:39:23 +0300

    test files

---
+++ src/providers/email/notificationCatcher.test.ts
@@ -0,0 +1,48 @@
+import NotifmeSdk from '../../../src';
+
+const mockSend = jest.fn();
+jest.mock('../../../src/providers/email/smtp', () => () => ({
+  send: mockSend
+}));
+
+const sdk = new NotifmeSdk({
+  useNotificationCatcher: true
+});
+
+const request = {
+  email: {
+    from: 'me@example.com',
+    to: 'john@example.com',
+    subject: 'Hi John',
+    html: '<b>Hello John! How are you?</b>',
+    text: 'Hello John! How are you?',
+    replyTo: 'contact@example.com'
+  }
+};
+
+test('email notification catcher provider should use SMTP provider.', async () => {
+  const result = await sdk.send(request);
+  const {to, from, html, text, subject, replyTo} = request.email;
+  expect(mockSend).lastCalledWith({
+    to, from, html, text, subject, replyTo, headers: {'X-to': `[email] ${to}`}
+  });
+  expect(result).toEqual({
+    status: 'success',
+    channels: {
+      email: {id: undefined, providerId: 'email-notificationcatcher-provider'}
+    }
+  });
+});
+
+test('email notification catcher provider should customize requests.', async () => {
+  await sdk.send({
+    email: {
+      ...request.email,
+      customize: async (provider, request) => ({...request, subject: 'Hi John!'})
+    }
+  });
+  const {to, from, html, text, replyTo} = request.email;
+  expect(mockSend).lastCalledWith({
+    to, from, html, text, subject: 'Hi John!', replyTo, headers: {'X-to': `[email] ${to}`}
+  });
+});
